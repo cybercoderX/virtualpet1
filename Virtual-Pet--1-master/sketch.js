268d8251893a6b45ee19bddea9192b6e24af2e53
cybercoderX <73379395+cybercoderX@users.noreply.github.com>
2021-01-06 09:55:38 +0530

    Add files via upload

---
+++ Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,25 @@
+//Create variables here
+var dog,happyDog,foodS,foodStock,dogImage;
+
+function preload()
+{
+  dogImage.loadImage('images/dogImg.png')
+  dogImage.loadImage('images/dogImg1.png')
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
